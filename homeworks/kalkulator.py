807a176040b10ac838fd97ee423febabf8d488e1
Karolina Krajewska <karolina.k.krajewska@gmail.com>
2020-08-08 15:51:01 +0200

    'Homework'

---
+++ homeworks/kalkulator.py
@@ -0,0 +1,38 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+
+print('Wybierz operację.')
+print('1.Dodawanie')
+print('2.Odejmowanie')
+print('3.Mnożenie')
+print('4.Dzielenie')
+
+while True:
+    choice = input('Wybierz operację(1/2/3/4): ')
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input('Podaj pierwszą liczbę: '))
+        num2 = float(input('Podaj drugą liczbę: '))
+        text = 'Wynik to: '
+
+        if choice == '1':
+            print(text, num1 + num2)
+        elif choice == '2':
+            print(text, num1 - num2)
+        elif choice == '3':
+            print(text, num1 * num2)
+        elif choice == '4':
+            print(text, num1 / num2)
+        break
+    else:
+        print('Błędna wartość, podaj poprawną')
