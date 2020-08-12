c21a768bc406ec7b3aaebc0f68cf24a63b689ebe
Karolina Krajewska <karolina.k.krajewska@gmail.com>
2020-08-12 17:09:19 +0200

    Koniec gry

---
+++ training/Koniec gry.py
@@ -0,0 +1,3 @@
+print ("Koniec gry")
+# \n\n wstawia dodatkowe odstępy a input służy do wprowadzania zmiennych przez użytkownika programu
+input("\n\nAby zakończyć program, naciśnij klawisz Enter.")
