9ad73da78608d555ba631078984c6cdf3ffcf295
Karolina Krajewska <karolina.k.krajewska@gmail.com>
2020-08-24 21:28:46 +0200

    modul6

---
+++ homeworks/Modul6.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome('/Users/Administrator/Downloads/fabryka/chromedriver.exe')
+driver.get('https://fabrykatestow.pl')
+button = driver.find_element_by_id('menu-item-506')
+button.click()
+driver.execute_script("window.scrollTo(0, 2300);")
+driver.get_screenshot_as_file("screenshot.png")
+driver.quit()
+
+
