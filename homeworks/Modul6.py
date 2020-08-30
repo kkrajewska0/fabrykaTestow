431bf4c9a14062a132c096f567454a36bc118035
pzwierzchowski <@WWL58kw>
2020-08-30 17:14:31 +0200

    add maximize_window

--- homeworks/Modul6.py
+++ homeworks/Modul6.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 
 driver = webdriver.Chrome('/Users/Administrator/Downloads/fabryka/chromedriver.exe')
 driver.get('https://fabrykatestow.pl')
+driver.maximize_window()
 button = driver.find_element_by_id('menu-item-506')
 button.click()
 driver.execute_script("window.scrollTo(0, 2300);")
