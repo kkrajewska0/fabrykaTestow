667d82abbc3746530f98840140323705ca01e6e6
Karolina Krajewska <karolina.k.krajewska@gmail.com>
2020-10-08 20:06:28 +0200

    screen

---
+++ homeworks/Simpletest/tests/page_objects/iFrame_page.py
@@ -0,0 +1,28 @@
+from homeworks.Simpletest.tests.helpers.support_functions import *
+
+iframe_tab = 'iframe-header'
+iframe_content = 'iframe-content'
+button_1 = 'simpleButton1'
+button_2 = 'simpleButton2'
+iframe_button = 'whichButtonIsClickedMessage'
+
+def click_iframe_tab(driver_instance):
+    wait_for_visibility_of_element(driver_instance, iframe_tab)
+    elem = driver_instance.find_element_by_id(iframe_tab)
+    elem.click()
+
+def iframe_content_displayed(driver_instance):
+    elem = wait_for_visibility_of_element(driver_instance, iframe_content)
+    return elem.is_displayed()
+
+def iframe(driver_instance):
+    frame = driver_instance.find_element_by_tag_name('iframe')
+    driver_instance.switch_to.frame(frame)
+
+def click_button_2(driver_instance):
+    elem = driver_instance.find_element_by_id(button_2)
+    elem.click()
+
+def button_clicked_displayed(driver_instance):
+    elem = wait_for_visibility_of_element(driver_instance, iframe_button)
+    return elem.is_displayed()
---
+++ homeworks/Simpletest/tests/page_objects/key_presses_page.py
@@ -0,0 +1,25 @@
+from homeworks.Simpletest.tests.helpers.support_functions import *
+from selenium.webdriver.common.keys import Keys
+
+key_presses_tab = 'keypresses-header'
+key_presses_content = 'keypresses-content'
+key_presses_input = 'target'
+result_key = 'keyPressResult'
+
+def click_presses_tab(driver_instance):
+    elem = driver_instance.find_element_by_id(key_presses_tab)
+    elem.click()
+
+def key_presses_content_visible(driver_instance):
+    elem = wait_for_visibility_of_element(driver_instance, key_presses_content)
+    return elem.is_displayed()
+
+def key_presses_tab2(driver_instance):
+    wait_for_visibility_of_element(driver_instance, key_presses_content, time_to_wait=1)
+    elem = driver_instance.find_element_by_id(key_presses_input)
+    elem.send_keys(Keys.ENTER)
+    result = driver_instance.find_element_by_id(result_key).get_attribute("outerHTML")
+    if result == '<p id="keyPressResult" style="color:green">You entered: ENTER</p>':
+        return True
+    else:
+        return False
